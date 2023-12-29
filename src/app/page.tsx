c4881d79d2d2c9ee544ac3e54493f0c0ed43cb91
cjcameron92 <cjcameron92@gmail.com>
2023-12-29 15:21:18 -0500

    added: bg updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,13 @@ import Logo from "../assets/SKYTOPIAlogo1.png"
 
 export default function Home() {
     return (
-        <main className="bg-[url('https://media.discordapp.net/attachments/886030285874003978/1190382496718794822/finally-14.png?ex=65a19917&is=658f2417&hm=b639e847f5637e7073655b7924511890484e8f0632a618339013adf1e7c3681e&=&format=webp&quality=lossless&width=3033&height=1705')] h-screen bg-cover bg-center flex flex-col items-center justify-center">
-            <div className="mb-4">
-                {/* Replace 'path_to_your_logo.png' with your actual logo image path */}
-                <Image src={Logo} alt="Logo" width={400} height={400} />
-            </div>
+        <main className="bg-[url('https://media.discordapp.net/attachments/886030285874003978/1190382043650068670/finally-17.png?ex=65a198ab&is=658f23ab&hm=3bc64ab14bf56ca32af91d49ce228e6208f7796d33b2b401f5072c1d03e7ac92&=&format=webp&quality=lossless&width=3033&height=1705')] h-screen bg-cover bg-center flex flex-col items-center justify-center">
+            {/*<div className="mb-4">*/}
+            {/*    /!* Replace 'path_to_your_logo.png' with your actual logo image path *!/*/}
+            {/*    <Image src={Logo} alt="Logo" width={200} height={200} />*/}
+            {/*</div>*/}
 
-            <div className="flex flex-col space-y-2 md:flex-row md:space-y-0 md:space-x-2">
+            <div className="flex flex-row items-center space-x-2 mt-64">
                 <a href="https://discord.gg/hqsSBhMzpr">
                     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-5 rounded">
                         Discord
